2e7171c0574c11d5679f978d883129633e78f0e2
MichiBaum <michael_baumberger@gmx.ch>
2020-06-10 21:31:07 +0200

    Update init-letsencrypt.sh
    lets try again

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -67,7 +67,8 @@ esac
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
 docker-compose run --rm --entrypoint "\
-  certbot certonly --webroot \
+  mkdir -p /var/www/certbot && \
+  certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
     $domain_args \
