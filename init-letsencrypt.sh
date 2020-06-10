71fb7f4a6f6fc668788870f6147e386ffadae544
MichiBaum <michael_baumberger@gmx.ch>
2020-06-10 20:10:30 +0200

    Update init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,11 +5,11 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(michibaum.ch www.michibaum.ch)
+domains=(michibaum.ch)
 rsa_key_size=4096
 data_path="/docker/certbot"
 email="michael_baumberger@gmx.ch"
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
