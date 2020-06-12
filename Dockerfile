4b26d449b01f73fd50160c7a50dbee14f0fd5e07
MichiBaum <michael_baumberger@gmx.ch>
2020-06-12 15:17:03 +0200

    maybe this time

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,9 @@ RUN rm -f /var/log/nginx/*
 
 RUN mkdir -p /etc/letsencrypt/live/michibaum.ch/
 
+COPY ./options-ssl-nginx.conf /etc/letsencrypt/options-ssl-nginx.conf
+COPY ./ssl-dhparams.pem /etc/letsencrypt/ssl-dhparams.pem
+
 COPY ./lifemanagement.conf /etc/nginx/conf.d/lifemanagement.conf
 COPY ./nginx.conf /etc/nginx/nginx.conf
 
--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -19,13 +19,13 @@ if [ -d "$data_path" ]; then
 fi
 
 
-if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
-  echo "### Downloading recommended TLS parameters ..."
-  mkdir -p "$data_path/conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
-  echo
-fi
+#if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
+#  echo "### Downloading recommended TLS parameters ..."
+#  mkdir -p "$data_path/conf"
+#  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+#  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+#  echo
+#fi
 
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
@@ -67,7 +67,7 @@ esac
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
 docker-compose run --rm --entrypoint "\
-  certbot certonly --webroot -w /var/www/certbot \
+  certbot certonly -a webroot -v --debug-challenges -w /var/www/certbot \
     $staging_arg \
     $email_arg \
     $domain_args \
